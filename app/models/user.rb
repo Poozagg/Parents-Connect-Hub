1aaaf4b6a0ab40998882319ad925d140fec34fcd
Pooja Gurung <pooja_grg07@hotmail.com>
2024-02-08 16:31:21 +0000

    working on the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :firstname, :lastname, :location, :no_of_children, :children_age_range, presence: true
 end
